5d12d66977b65165c44dc3e6a8bb24254c25f570
profftols <85066262+profftols@users.noreply.github.com>
2024-08-01 16:21:49 +0500

    Работа 01.08
    Новое:
    - класс слежение камеры
    - папки
    
    Обновлено:
    - удалил краситель
    - перетащил классы в папки

---
+++ Assets/SeizureTerritory/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _smoothSpeed = 0.125f;
+    
+    private void FixedUpdate()
+    {
+        var desiredPosition = _target.position;
+        var smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, _smoothSpeed);
+        transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, smoothedPosition.z);
+    }
+}
